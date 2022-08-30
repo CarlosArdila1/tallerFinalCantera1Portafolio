827b3137452360c271548513cc17bb59cfa5fd76
Carlos Fernando Ardila Marin <carlos.ardila.tres@gmail.com>
2022-08-30 14:32:45 -0500

    Final cantera 1- portafolio
    Subo el punto 3 del taller final de cantera 1 de HTML, CSS y JS

---
+++ cv/JS/archivo.js
@@ -0,0 +1,81 @@
+const main = () => {
+    
+}
+
+const colorChanger = () => {
+    var selectValue = document.getElementById("selection").value;
+    document.querySelector(".mainColor").style.backgroundColor = selectValue;
+    document.querySelector(".headerColor").style.backgroundColor = selectValue;
+    document.querySelector(".footerColor").style.backgroundColor = selectValue;
+}
+
+
+const cerrar = () => {
+    div = document.querySelector(".flotante");
+    div.style.display = 'none';
+}
+
+const mostrar = () => {
+    div = document.querySelector(".flotante");
+    div.style.display = '';
+}
+
+const cerrar2 = () => {
+    div = document.querySelector(".flotante2");
+    div.style.display = 'none';
+}
+
+const mostrar2 = () => {
+    div = document.querySelector(".flotante2");
+    div.style.display = '';
+}
+
+const cerrar3 = () => {
+    div = document.querySelector(".flotante3");
+    div.style.display = 'none';
+}
+
+const mostrar3 = () => {
+    div = document.querySelector(".flotante3");
+    div.style.display = '';
+}
+
+const cerrar4 = () => {
+    div = document.querySelector(".flotante4");
+    div.style.display = 'none';
+}
+
+const mostrar4 = () => {
+    div = document.querySelector(".flotante4");
+    div.style.display = '';
+}
+
+const close1 = () => {
+    div = document.querySelector(".float1");
+    div.style.display = 'none';
+}
+
+const open1 = () => {
+    div = document.querySelector(".float1");
+    div.style.display = '';
+}
+
+const close2 = () => {
+    div = document.querySelector(".float2");
+    div.style.display = 'none';
+}
+
+const open2 = () => {
+    div = document.querySelector(".float2");
+    div.style.display = '';
+}
+
+const close3 = () => {
+    div = document.querySelector(".float3");
+    div.style.display = 'none';
+}
+
+const open3 = () => {
+    div = document.querySelector(".float3");
+    div.style.display = '';
+}
